60b7f7ddb3fba7eb965648dec30fb433e0aea335
YaelRevayev <gexploreapp@gmail.com>
2024-03-21 10:27:15 +0200

    need to change loffings

--- http_server.py
+++ http_server.py
@@ -5,23 +5,24 @@ from typing import List
 
 app = FastAPI()
 
-UPLOAD_DIR = "./received-images" 
+UPLOAD_DIR = "./received-images"
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+log_file = "http_server_log.txt"
+logging.basicConfig(filename=log_file, level=logging.INFO)
+
 
 @app.post("/upload/")
 async def upload_files(files: List[UploadFile] = File(...)):
     if not files:
-        logger.error("No files received")
-
+        logging.error("No files received")
     for uploaded_file in files:
         file_path = os.path.join(UPLOAD_DIR, uploaded_file.filename)
         with open(file_path, "wb") as file_object:
             file_object.write(await uploaded_file.read())
-            logger.info("A file is created.")
+            logging.info("A file is created.")
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
